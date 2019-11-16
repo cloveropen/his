7ed050d9810acf63b915541c0463d76d95dd0c9c
cloveropen.com <cloveropen@163.com>
2019-11-16 12:59:46 +0800

    change mainpage

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,31 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span><a href="http://www.cloveropen.com/">苜蓿草科技</a></span>
+    <v-app-bar app style="background-color:darkgray" height="60">
+      <span><v-img :src="require('./assets/logo.svg')"  contain height="70"  width="60"  position="left top"></v-img>
+      </span>
+      <v-toolbar-title class="headline text-uppercase" >
+      
+        <span ><a href="http://www.cloveropen.com/" style="color:antiquewhite">苜蓿草科技</a></span>
         <!-- <span class="font-weight-light">&nbsp;&nbsp;www.cloveropen.com</span>  -->
       </v-toolbar-title>
       <v-spacer></v-spacer>
 
         <span class="mr-2">v0.0.1</span>
-      </v-btn>
+  
+
     </v-app-bar>
 
-    <v-content>
+    <v-content style="background-color:darkgray" >
+     
       <HelloWorld/>
+     
     </v-content>
+    
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld';
-
 export default {
   name: 'App',
   components: {
@@ -30,3 +36,9 @@ export default {
   }),
 };
 </script>
+
+<style  lang='scss'>
+.mr-2{
+  color:antiquewhite;
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,30 @@
 <template>
   <v-container fluid grid-list-lg>
-    <v-layout text-center wrap>
-      <v-flex xs12>
-        <v-img :src="require('../assets/logo.svg')" class="my-3" contain height="200"></v-img>
-      </v-flex>
-
-      <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">智慧医院信息平台</h1>
+    <v-layout text-center wrap >
+      <v-carousel
+    cycle
+    height="450"
+    hide-delimiter-background
+    show-arrows-on-hover
+  >
+  
+      <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+    >
+     </v-carousel-item>
+ 
+  </v-carousel>
+   
+       <v-flex mb-4 style="background-color:gray"><v-img src="../assets/5.jpg">  
+        <h2 class="display-2 font-weight-bold mb-1" >智慧医院信息平台</h2>
         <p class="subheading font-weight-regular">以电子病历为核心的智慧医院信息平台</p>
+      </v-img>
       </v-flex>
-
-      <v-flex mb-5 xs12>
-        <h2 class="headline font-weight-bold mb-3">医疗管理系统</h2>
+   
+      <v-flex mb-5 xs4 ><v-img src="../assets/8.jpg"> 
+        <h1 class="headline font-weight-bold mb-3" >医疗管理系统</h1>
 
         <v-layout justify-center wrap>
           <v-list-item>
@@ -22,15 +35,17 @@
                   :key="i"
                   :href="next.href"
                   class="subheading mx-3"
-                  target="_blank"
-                >{{ next.text }}</a>
+                  target="_blank" 
+                  style="color:burlywood"
+                >{{ next.text }}<br></a>
               </v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </v-layout>
+      </v-img>
       </v-flex>
 
-      <v-flex xs12 mb-5>
+      <v-flex xs4 mb-5 ><v-img src="../assets/6.jpg"> 
         <h2 class="headline font-weight-bold mb-3">临床服务系统</h2>
 
         <v-layout justify-center row wrap>
@@ -44,15 +59,17 @@
                   :href="link.href"
                   class="subheading mx-3"
                   target="_blank"
-                >{{ link.text }}</a>
+                  style="color:black"
+                >{{ link.text }}<br></a>
               </v-list-item-title>
             </v-list-item-content>
           </v-list-item>
           </v-flex>
         </v-layout>
+        </v-img>
       </v-flex>
 
-      <v-flex xs12 mb-5>
+      <v-flex xs4 mb-5  ><v-img src="../assets/7.jpg" > 
         <h2 class="headline font-weight-bold mb-3">运营管理系统</h2>
 
         <v-layout justify-center row wrap>
@@ -66,32 +83,60 @@
                   :href="eco.href"
                   class="subheading mx-3"
                   target="_blank"
-                >{{ eco.text }}</a>
+                    style="color:burlywood"
+                >{{ eco.text }}<br></a>
               </v-list-item-title>
             </v-list-item-content>
           </v-list-item>
           </v-flex>
         </v-layout>
+        </v-img>
       </v-flex>
+      <v-flex xs12>
+  <v-footer
+    style="background-color:darkgray"
+  >
+      <v-card-title  style="width:100%">
+          <v-flex xs4>
+        <strong class="subheading" style="color:antiquewhite">沈阳市苜蓿草科技有限公司</strong>
+          </v-flex>
+          <v-flex xs8>
+         <span style="color:black;msrgin-right'50px'" ><h5>  电话：024-62571157   地址：沈阳市皇姑区万众商务大厦14-5   邮箱： eastwill.net@126.com</h5></span>
+          </v-flex>
+      </v-card-title>
+       
+  </v-footer>
+ 
+  </v-flex>
     </v-layout>
-  </v-container>
+  </v-container> 
 </template>
 
 <script>
+import a from "../assets/2.jpg";
+import b from "../assets/2.jpeg";
+import c from "../assets/3.jpg";
+import d from "../assets/4.jpg";
 export default {
   data: () => ({
+    items: [
+          {
+            src: b,
+          },
+          {
+            src:a,
+          },
+          {
+            src: c,
+          },
+          {
+            src: d,
+          },
+        ],
     operations: [
       {
         text: "人力资源管理",
         href: "http://www.cloveropen.com"
-      },
-      {
-        text: "财务管理",
-        href: "https://cloveropen.app-hos.com:9090/"
-      },
-      {
-        text: "药品管理",
-        href: "https://cloveropen.app-hos.com:9091/"
       },
       {
         text: "设备材料管理",
@@ -104,35 +149,23 @@ export default {
       {
         text: "预算管理",
         href: "http://www.cloveropen.com"
+      },
+       {
+        text: "财务管理",
+        href: "https://cloveropen.app-hos.com:9090/"
+      },
+      {
+        text: "药品管理",
+        href: "https://cloveropen.app-hos.com:9091/"
       }
     ],
     healthCare: [
       {
-        text: "门急诊收费系统",
-        href: "https://cloveropen.app-hos.com:9081/"
-      },
-      {
-        text: "住院收费系统",
-        href: "https://cloveropen.app-hos.com:9082"
-      },
-      {
-        text: "护理管理系统",
-        href: "https://cloveropen.app-hos.com:8601"
-      },
-      {
-        text: "医务管理系统",
+        text: "食源性疾病上报系统接口",
         href: "http://www.cloveropen.com"
       },
-      {
+       {
         text: "院感/传染病管理系统",
-        href: "https://cloveropen.app-hos.com:8601"
-      },
-      {
-        text: "科研教学管理系统",
-        href: "http://www.cloveropen.com"
-      },
-      {
-        text: "病案管理系统",
         href: "http://www.cloveropen.com"
       },
       {
@@ -144,24 +177,42 @@ export default {
         href: "http://www.cloveropen.com"
       },
       {
-        text: "食源性疾病上报系统接口",
+        text: "科研教学管理系统",
+        href: "http://www.cloveropen.com"
+      },
+      {
+        text: "门急诊收费系统",
+        href: "https://cloveropen.app-hos.com:9081/"
+      },
+      {
+        text: "住院收费系统",
+        href: "https://cloveropen.app-hos.com:9082"
+      },
+      {
+        text: "护理管理系统",
+        href: "https://cloveropen.app-hos.com:9085"
+      },
+      {
+        text: "医务管理系统",
+        href: "http://www.cloveropen.com"
+      },
+     
+      {
+        text: "病案管理系统",
         href: "http://www.cloveropen.com"
       }
     ],
     clinicSrv: [
       {
-        text: "门急诊挂号收费",
-        href: "https://cloveropen.app-hos.com:9081/"
+        text: "电子病历书写与管理",
+        href: "https://cloveropen.app-hos.com:9087"
       },
       {
-        text: "住院病人入出转",
-        href: "https://cloveropen.app-hos.com:9082"
-      },
-      {
-        text: "分诊管理",
+        text: "超声/内镜/病理管理",
         href: "http://www.cloveropen.com"
       },
-      {
+      
+       {
         text: "门诊医生工作站",
         href: "https://cloveropen.app-hos.com:9083"
       },
@@ -174,23 +225,15 @@ export default {
         href: "https://cloveropen.app-hos.com:9085"
       },
       {
-        text: "电子病历书写与管理",
-        href: "https://cloveropen.app-hos.com:9087"
+        text: "门急诊挂号收费",
+        href: "https://cloveropen.app-hos.com:9081/"
+      },
+      {
+        text: "住院病人入出转",
+        href: "https://cloveropen.app-hos.com:9082"
       },
       {
         text: "合理用药管理",
-        href: "http://www.cloveropen.com"
-      },
-      {
-        text: "临床检验(LIS)",
-        href: "http://www.cloveropen.com"
-      },
-      {
-        text: "医学影像(PACS)",
-        href: "http://www.cloveropen.com"
-      },
-      {
-        text: "超声/内镜/病理管理",
         href: "http://www.cloveropen.com"
       },
       {
@@ -201,6 +244,19 @@ export default {
         text: "临床路径管理",
         href: "http://www.cloveropen.com"
       },
+      {
+        text: "医学影像(PACS)",
+        href: "http://www.cloveropen.com"
+      },
+      {
+        text: "临床检验(LIS)",
+        href: "http://www.cloveropen.com"
+      },
+      {
+        text: "分诊管理",
+        href: "http://www.cloveropen.com"
+      },
+      
       {
         text: "输血管理",
         href: "https://cloveropen.app-hos.com:9088"
@@ -221,3 +277,17 @@ export default {
   })
 };
 </script>
+<style lang="scss" scoped>
+.mb-3{
+  color:darkkhaki;
+}
+a{
+  text-decoration: none;
+  font-size: 20px
+}
+.mb-5{
+  background-color: darkgray;
+ 
+}
+
+</style>
